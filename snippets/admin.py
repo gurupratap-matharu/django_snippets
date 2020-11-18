4e7613af525e40c6919819f2f81d7ed5d2e34c44
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-18 15:48:08 -0300

    added snippet and language to django admin interface

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from .models import Language
+from .models import Language, Snippet
 
 
-@admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('name',)
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'public', 'language', 'user', 'created',)
+    list_filter = ('public', 'created', 'updated',)
+    list_editable = ('public',)
+    search_fields = ('name', 'description', 'snippet',)
+    raw_id_fields = ('user',)
+    date_hierarchy = 'created'
+    ordering = ('public', 'created')
+
+
+admin.site.register(Language, LanguageAdmin)
+admin.site.register(Snippet, SnippetAdmin)
