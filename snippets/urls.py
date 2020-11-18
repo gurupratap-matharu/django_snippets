2222381895f40877eba34cabbe4e45a267abc185
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-18 17:23:23 -0300

    replaced function views by class based views

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('snippets/python/', views.language, name='language'),
-    path('snippets/user/juancito/', views.user_snippets, name='user_snippets'),
-    path('snippets/snippet/', views.snippet, name='snippet'),
-    path('snippets/add/', views.snippet_add, name='snippet_add'),
-    path('snippets/edit/', views.snippet_edit, name='snippet_edit'),
-    path('snippets/delete/', views.snippet_delete, name='snippet_delete'),
+    path('snippets/python/', views.LanguageView.as_view(), name='language'),
+    path('snippets/user/juancito/', views.UserSnippets.as_view(), name='user_snippets'),
+    path('snippets/snippet/', views.SnippetDetailView.as_view(), name='snippet'),
+    path('snippets/add/', views.SnippetCreate.as_view(), name='snippet_add'),
+    path('snippets/edit/', views.SnippetUpdate.as_view(), name='snippet_edit'),
+    path('snippets/delete/', views.SnippetDelete.as_view(), name='snippet_delete'),
 ]
