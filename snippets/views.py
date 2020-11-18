2362ade5f690ea0c582ed6062c49aef6e2f4f73b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-18 17:23:47 -0300

    implemented basic crud operation on snippet model

--- snippets/views.py
+++ snippets/views.py
@@ -1,4 +1,14 @@
+import logging
+
+from django.contrib import messages
 from django.shortcuts import render
+from django.urls.base import reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
+
+from snippets.models import Language, Snippet
+
+logger = logging.getLogger(__name__)
 
 
 def login(request):
@@ -9,29 +19,59 @@ def logout(request):
     return render(request, 'login.html', {})
 
 
-def index(request):
-    return render(request, 'index.html', {})
+class IndexView(ListView):
+    model = Snippet
+    template_name = 'index.html'
+    context_object_name = 'snippet_list'
+    paginate_by = 12
 
 
-def language(request):
-    return render(request, 'index.html', {})
+class LanguageView(ListView):
+    model = Snippet
+    context_object_name = 'snippet_list'
+    template_name = 'index.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        language = self.request.kwargs['language']
+        logger.info('language: %s', language)
+        return queryset.filter(language=language)
 
 
-def user_snippets(request):
-    return render(request, 'snippets/user_snippets.html', {})
+class UserSnippets(ListView):
+    model = Snippet
+    context_object_name = 'snippet_list'
+    template_name = 'snippets/user_snippets.html'
+    paginate_by = 12
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
 
 
-def snippet(request):
-    return render(request, 'snippets/snippet.html', {})
+class SnippetDetailView(DetailView):
+    model = Snippet
+    context_object_name = 'snippet'
+    template_name = 'snippets/snippet.html'
 
 
-def snippet_add(request):
-    return render(request, 'snippets/snippet_add.html', {})
+class SnippetCreate(CreateView):
+    model = Snippet
+    template_name = 'snippets/snippet_add.html'
 
 
-def snippet_edit(request):
-    return render(request, 'snippets/snippet_add.html', {})
+class SnippetUpdate(UpdateView):
+    model = Snippet
+    template_name = 'snippets/snippet_add.html'
 
 
-def snippet_delete(request):
-    return render(request, 'snippets/user_snippets.html', {})
+class SnippetDelete(DeleteView):
+    model = Snippet
+    template_name = 'snippets/user_snippets.html'
+    success_url = reverse_lazy('user_snippets')
+    success_message = 'Snippet deleted successfully!'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(SnippetDelete, self).delete(request, *args, **kwargs)
