d81e78898eb88ad4f30fe7489d5957a5058b780d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-18 14:46:23 -0300

    added language andsnippet models

--- snippets/models.py
+++ snippets/models.py
@@ -1,11 +1,33 @@
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Language(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Snippet(models.Model):
-    pass
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='snippets')
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name='snippets')
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    snippet = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    public = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f"Snippet {self.name} in {self.language} by {self.user} "
+
+    def get_absolute_url(self):
+        return reverse("snippets:snippet_detail", args=[self.language.slug, self.name])
