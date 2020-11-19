d5d0292f0698357f22005a660c1acbc44c340702
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-19 11:55:51 -0300

    reordered the path

--- snippets/urls.py
+++ snippets/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('python/', views.LanguageView.as_view(), name='language'),
-    path('user/juancito/', views.UserSnippets.as_view(), name='user_snippets'),
-    path('snippet/<uuid:pk>/', views.SnippetDetailView.as_view(), name='snippet'),
+    path('<slug:language>/', views.LanguageView.as_view(), name='language'),
+    path('user/<slug:user>/', views.UserSnippets.as_view(), name='user_snippets'),
     path('add/', views.SnippetCreate.as_view(), name='snippet_add'),
+    path('<uuid:pk>/', views.SnippetDetailView.as_view(), name='snippet'),
     path('<uuid:pk>/edit/', views.SnippetUpdate.as_view(), name='snippet_edit'),
     path('<uuid:pk>/delete/', views.SnippetDelete.as_view(), name='snippet_delete'),
 ]
