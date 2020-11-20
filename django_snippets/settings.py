0fad2cae4f7265c23833b1357d8cf0e4da836ee3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-19 22:59:15 -0300

    added heroku domain to allowed hosts

--- django_snippets/settings.py
+++ django_snippets/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver', '0.0.0.0']
+ALLOWED_HOSTS = ['django-snippets.herokuapp.com', 'localhost', '127.0.0.1', 'testserver', '0.0.0.0']
 
 
 # Application definition
@@ -100,8 +100,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
