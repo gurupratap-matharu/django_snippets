07537e8c29b86c82edadf2a775957ab610366c57
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-21 17:31:36 -0300

    added model form for snippet

---
+++ snippets/forms.py
@@ -0,0 +1,25 @@
+import logging
+
+from django import forms
+from django.core.mail import send_mail
+
+from snippets.models import Snippet
+
+logger = logging.getLogger(__name__)
+
+
+class SnippetForm(forms.ModelForm):
+    class Meta:
+        model = Snippet
+        fields = ['name', 'language', 'description', 'snippet', 'public']
+        widget = {
+            'description': forms.Textarea(attrs={'cols': 80, 'rows': 3})
+        }
+
+    def send_mail(self, **kwargs):
+        user_email = kwargs.get('email')
+        logger.info('sending snippet creation email to %s...', user_email)
+        message = "Thank you for creating a snippet."
+        send_mail(subject='Site message', message=message, from_email='site@domain.com',
+                  recipient_list=['gurupratap.matharu@gmail.com', user_email],
+                  fail_silently=False)
