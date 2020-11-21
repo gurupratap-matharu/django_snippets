4faf688a24048bd34d6b3e473113ac693fd41fb4
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-11-21 19:12:58 -0300

    reverted to default pygmentify settings

--- snippets/templatetags/snippets_tags.py
+++ snippets/templatetags/snippets_tags.py
@@ -1,5 +1,9 @@
 from django import template
 from django.utils import timezone
+from django.utils.safestring import mark_safe
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
 
 register = template.Library()
 
@@ -9,3 +13,11 @@ def days_since(created):
     dt = timezone.now() - created + timezone.timedelta(days=1)
     days = str(dt.days)
     return days
+
+
+@register.filter()
+def lexify(language, code):
+    lexer = get_lexer_by_name(language, stripall=True)
+    formatter = HtmlFormatter(linenos=True, cssclass="source")
+    result = highlight(code, lexer, formatter)
+    return mark_safe(result)
